2da7a98ad485c7e378b658b6b1a4fb768ad8dfb6
Debadrita Mondal <111679707+mondaldebadrita03@users.noreply.github.com>
2026-08-24 23:06:53 +0530

    Create 41A-Translation.java

---
+++ 41A-Translation.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Translation{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s1 = sc.nextLine();
+		String s2 = sc.nextLine();
+		if(s1.length() != s2.length()){
+		    System.out.println("NO");
+		    return;
+		}
+		int j = s2.length() - 1;
+		
+		for(int i = 0; i < s1.length(); i++){
+		    if(s1.charAt(i) != s2.charAt(j)){
+		        System.out.println("NO");
+		        return;
+		    }
+		    j--;
+		}
+		System.out.println("YES");
+	}
+}
