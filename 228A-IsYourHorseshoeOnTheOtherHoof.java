02e930f14d0bb9fa8f5312ab61acbe07dc3a27bc
Debadrita Mondal <111679707+mondaldebadrita03@users.noreply.github.com>
2026-09-05 01:53:31 +0530

    Update 228A-IsYourHorseshoeOnTheOtherHoof.java

--- 228A-IsYourHorseshoeOnTheOtherHoof.java
+++ 228A-IsYourHorseshoeOnTheOtherHoof.java
@@ -16,3 +16,18 @@ public class IsYourHorseshoeOnTheOtherHoof{
 		System.out.println(count);
 	}
 }
+
+// Another version (Cleaner)
+import java.util.*;
+public class IsYourHorseshoeOnTheOtherHoof{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		HashSet<Integer> set = new HashSet<>();
+		
+		for(int i= 0; i < 4; i++){
+		    set.add(sc.nextInt());
+		}
+		System.out.println(4 - set.size());
+	}
+}
+
