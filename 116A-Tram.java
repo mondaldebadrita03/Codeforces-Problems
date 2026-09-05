db8acfaf1a6b09d2de24bcaa29ee23696f3faae5
Debadrita Mondal <111679707+mondaldebadrita03@users.noreply.github.com>
2026-09-06 01:43:53 +0530

    Create 116A-Tram.java

---
+++ 116A-Tram.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Tram{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int current = 0;
+		int max = 0;
+		
+		for(int i = 0; i < n; i++){
+		    int a = sc.nextInt();
+		    int b = sc.nextInt();
+		    current = current - a + b;
+		    
+		    max = Math.max(max, current);
+		}
+		System.out.println(max);
+	}
+}
