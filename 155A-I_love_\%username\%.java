209ea7f1d8c6333cadb1f3058b0726137220d900
Debadrita Mondal <111679707+mondaldebadrita03@users.noreply.github.com>
2026-09-20 19:37:46 +0530

    Create 155A-I_love_\%username\%.java

---
+++ 155A-I_love_\%username\%.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		
+		for(int i = 0; i < n ; i++){
+		    arr[i] = sc.nextInt();
+		}
+		
+		int max = Integer.MIN_VALUE;
+		int min = Integer.MAX_VALUE;
+		int count = 0;
+		
+		for(int i = 0; i < n - 1; i++){
+		    max = Math.max(max, arr[i]);
+		    min = Math.min(min, arr[i]);
+		    
+		    if(arr[i + 1] > max || arr[i + 1] < min)
+		        count++;
+		}
+		System.out.println(count);
+	}
+}
